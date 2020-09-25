7eb5ee600ded30c498f50d1260556f6d5fdad83a
syed <syedmatheen55@gmail.com>
2020-09-25 18:40:37 +0530

    Add files via upload

---
+++ EmailNotification.java
@@ -0,0 +1,9 @@
+public class EmailNotification implements Notification{
+
+	@Override
+	public void notifyUser() {
+		// TODO Auto-generated method stub
+		System.out.println("Sending a Email notification"); 
+	}
+
+}
